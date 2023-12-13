cc95738991313003ba9bf1993f95701d005dd0e4
Anciappi <ciappiadrian@gmail.com>
2023-12-13 10:04:34 -0300

    version pythonanywhere

--- tierraWines/settings.py
+++ tierraWines/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--&4p^)s+s4j9fc!jhln0^%eu1hm)^u6j-x=$&+-jy0v4_)tplr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
